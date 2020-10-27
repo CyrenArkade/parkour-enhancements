bae82f9e23040743d53806fa567de062874cbc51
CyrenArkade <joshuatrapp@gmail.com>
2020-10-26 21:14:29 -0700

    updated ver num
    ver num

--- parkour-enhancements/src/main/java/com/cyren/mod/NGGlobal.java
+++ parkour-enhancements/src/main/java/com/cyren/mod/NGGlobal.java
@@ -4,7 +4,7 @@ public class NGGlobal {
 
 	public static final String MOD_ID = "parkourenhancements";
 	public static final String MOD_NAME = "Parkour Enhancements";
-	public static final String VERSION = "0.2.4";
+	public static final String VERSION = "0.2.5";
 	
 	public static final String NG_CLIENT_PROXY = "com.cyren.mod.proxy.ClientProxy";
 	public static final String NG_COMMON_PROXY = "com.cyren.mod.proxy.CommonProxy";
